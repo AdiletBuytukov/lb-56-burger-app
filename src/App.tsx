e1e1a867203d418d036f7703293a2c46e7f16c54
Adilet Buytukov <abuytukov@gmail.com>
2024-01-12 22:23:54 +0600

    Добавил файл App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
-import './App.css'
+import './App.css';
 import INGREDIENTS from './Components/ingredints';
+import {FreqMap, Ingredient} from './types';
+import React, {useState} from 'react';
+import IngredientList from './Components/ingredientList';
+import { Burger } from './Components/displayBurger'
 
+const initialIngredient: FreqMap = INGREDIENTS.reduce((acc, ing) => {
+  acc[ing.name] = 0;
+  return acc;
+}, {} as FreqMap);
 
-const App = () => {
-  const initialIngredient: Record<string, number> = INGREDIENTS.reduce((acc, ing) => {
-    acc[ing.name] = 0;
-    return acc;
-  }, {});
-}
+const defaultPrice = 30;
+
+const App: React.FC = () => {
+  const [ingCount, setIngCount] = useState(initialIngredient);
+
+  const addIng = (ingredient: Ingredient) => {
+    setIngCount((prevState) => ({
+      ...prevState,
+      [ingredient.name]: prevState[ingredient.name] + 1,
+    }));
+  };
+
+  const removeIng = (ingredient: Ingredient) => {
+    setIngCount((prevState) => ({
+      ...prevState,
+      [ingredient.name]: Math.max(prevState[ingredient.name] - 1, 0),
+    }));
+  };
+
+  const calcPrice = (): number => {
+    return INGREDIENTS.reduce((calcPrice, ingredient) => {
+      return calcPrice + ingredient.price * ingCount[ingredient.name];
+    }, 0);
+  };
+  return (
+    <div>
+      <p>Ингридиенты</p>
+      <div>
+        <IngredientList
+          ingredients={INGREDIENTS}
+          onClickAdd={(ingredient) => addIng(ingredient)}
+          onRemoveClick={(ingredient) => removeIng(ingredient)}
+        />
+      </div>
+      <Burger
+        ingredients={INGREDIENTS.filter((ingredient) => ingCount[ingredient.name] > 0)}
+      />
+      <p>Стоимость бургера: {defaultPrice + calcPrice()} сом</p>
+    </div>
+  );
+};
 
 export default App;
